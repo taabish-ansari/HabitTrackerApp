910b3811cfab0dad416262e2a523f2ebe192bde5
Taabish Ahmed Ansari <156155062+taabish-ansari@users.noreply.github.com>
2026-09-02 00:00:59 +0530

    style: load responsive polish layer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import ThemeToggle from './components/ThemeToggle';
 import './styles.css';
+import './responsive.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
