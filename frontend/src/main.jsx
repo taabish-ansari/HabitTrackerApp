4ac74235f62a4d886cb92c0354026789fe686c9b
Taabish Ahmed Ansari <156155062+taabish-ansari@users.noreply.github.com>
2026-09-01 23:59:31 +0530

    feat: integrate theme control

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ThemeToggle from './components/ThemeToggle';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    <App />
+    <div className="global-theme-control">
+      <ThemeToggle compact />
+    </div>
+  </React.StrictMode>
 );
