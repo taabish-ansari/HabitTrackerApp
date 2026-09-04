ae0040e02e487200db6983545cfe3a67ef2ca1b6
Taabish Ahmed Ansari <156155062+taabish-ansari@users.noreply.github.com>
2026-09-04 07:16:06 +0530

    Mount reward celebration experience

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import ProfileOverlay from './components/ProfileOverlay';
 import HabitDetailOverlay from './components/HabitDetailOverlay';
 import HabitScheduleManager from './components/HabitScheduleManager';
 import InsightsEnhancer from './components/InsightsEnhancer';
+import RewardCelebration from './components/RewardCelebration';
 import './styles.css';
 import './responsive.css';
 import './premium.css';
@@ -26,6 +27,7 @@ import './typography.css';
 import './insights.css';
 import './insights-enhancer.css';
 import './rewards.css';
+import './reward-celebration.css';
 import './insights-scroll-reset.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -35,6 +37,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <HabitDetailOverlay />
     <HabitScheduleManager />
     <InsightsEnhancer />
+    <RewardCelebration />
     <div className="global-theme-control">
       <ThemeToggle compact />
     </div>
