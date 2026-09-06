0a9a798283f23b858471318d78ee1fef29871047
Taabish Ahmed Ansari <156155062+taabish-ansari@users.noreply.github.com>
2026-09-06 10:38:24 +0530

    Remove personalized routine from Today

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,6 @@ import ProfileOverlay from './components/ProfileOverlay';
 import HabitDetailOverlay from './components/HabitDetailOverlay';
 import HabitScheduleManager from './components/HabitScheduleManager';
 import InsightsEnhancer from './components/InsightsEnhancer';
-import PersonalizedRoutine from './components/PersonalizedRoutine';
 import RewardCelebration from './components/RewardCelebration';
 import './styles.css';
 import './responsive.css';
@@ -47,7 +46,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <HabitDetailOverlay />
     <HabitScheduleManager />
     <InsightsEnhancer />
-    <PersonalizedRoutine />
     <RewardCelebration />
     <div className="global-theme-control">
       <ThemeToggle compact />
